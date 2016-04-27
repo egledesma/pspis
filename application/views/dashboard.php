eff4b201ecab5726d3e8d827ef0859ec06ac937c
mblejano <mblejano@dswd.gov.ph>
2016-04-27 11:23:40 +0800

    maxmin

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -81,7 +81,7 @@ $remaining_format =  substr($remaining,0,-1);
 
                     headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
                     pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-                    '<td style="padding:0"><b>₱ {point.y:,.1f} </b></td></tr>',
+                    '<td style="padding:0"><b>₱ {point.y:,.1f} </b>({point.percentage:.0f}%)</td></tr>',
                     footerFormat: '</table>',
                     shared: true,
                     useHTML: true
@@ -90,7 +90,8 @@ $remaining_format =  substr($remaining,0,-1);
                 plotOptions: {
                     column: {
                         pointPadding: 0.2,
-                        borderWidth: 0
+                        borderWidth: 0,
+
                     }
                 },
                 series: [{
@@ -105,15 +106,78 @@ $remaining_format =  substr($remaining,0,-1);
                     name: 'Remaining Balance',
                     data: [<?php echo $remaining_format;?>]
 
-                }, {
-                    name: 'Status',
-                    data: [<?php echo $status_format;?>]
-
                 }]
             });
         });
     </script>
     <div id="container" style="min-width: 310px; height: 400px; margin: 0 auto"></div>
+    <div id="container1" style="min-width: 310px; height: 400px; margin: 0 auto"></div>
 
+    <script type="text/javascript">
+        $(function () {
+            $('#container1').highcharts({
+                chart: {
+                    type: 'column'
+                },
+                title: {
+                    text: 'FUND MONITORING'
+                },
+                subtitle: {
+                    text: 'Allocated , Utilized and Remaining Balance'
+                },
+                xAxis: {
+                    categories: [<?php echo $region_format;?>],
+                },
+                yAxis: {
+                    min: 0,
+                    title: {
+                        text: 'Funds'
+                    },
+                    stackLabels: {
+                        enabled: true,
+                        style: {
+                            fontWeight: 'bold',
+                            color: (Highcharts.theme && Highcharts.theme.textColor) || 'gray'
+                        }
+                    }
+                },
+                legend: {
+                    align: 'right',
+                    x: -30,
+                    verticalAlign: 'top',
+                    y: 25,
+                    floating: true,
+                    backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || 'white',
+                    borderColor: '#CCC',
+                    borderWidth: 1,
+                    shadow: false
+                },
+                tooltip: {
+                    headerFormat: '<b>{point.x}</b><br/>',
+                    pointFormat: '{series.name}:₱ {point.y:,.1f}({point.percentage:.0f}%)<br/>Total: ₱ {point.stackTotal:,.0f}'
+                },
+                plotOptions: {
+                    column: {
+                        stacking: 'normal',
+                        dataLabels: {
+                            enabled: true,
+                            color: (Highcharts.theme && Highcharts.theme.dataLabelsColor) || 'white',
+                            style: {
+                                textShadow: '0 0 3px black'
+                            }
+                        }
+                    }
+                },
+                series: [ {
+                    name: 'Utilized',
+                    data: [<?php echo $util_format;?>]
 
+                }, {
+                    name: 'Remaining Balance',
+                    data: [<?php echo $remaining_format;?>]
+
+                }]
+            });
+        });
+</script>
 </div>
