b8723cb81c6d39582c89e162c9223346181176bb
mblejano <mblejano@dswd.gov.ph>
2016-06-06 17:06:05 +0800

    foodforwork saro checker

--- application/views/cofunds_list.php
+++ application/views/cofunds_list.php
@@ -107,9 +107,6 @@
                     &nbsp;<?php //echo $form_message; ?>
                 </header>
                 <div class="panel-body">
-                    <div id="exampleTableAddToolbar">
-                        <a class= "btn btn-outline btn-primary"   href="<?php echo base_url('cofunds/add') ?>"><i class="icon wb-plus" aria-hidden="true"></i> Add Record</a>
-                    </div><br>
                     <table class="table table-hover table-bordered dataTable table-striped width-full" data-plugin="dataTable">
                         <thead>
                         <tr>
@@ -176,18 +173,6 @@
                     </table>
                 </div>
 
-                <div class="modal fade" id="exampleFormModal1" aria-hidden="false" aria-labelledby="exampleFormModalLabel1"
-                     role="dialog" tabindex="-1">
-                    <div class="modal-dialog modal-center">
-                        <?php
-                        $attributes = array("class" => "modal-content", "id" => "cofunds_edit", "name" => "cofunds_edit");
-                        //input here the next location when click insert1
-                        echo form_open("cofunds/edit", $attributes);?>
-
-                        <?php echo form_close(); ?>
-                        <?php echo $this->session->flashdata('msg'); ?>
-                    </div>
-                </div>
 
             </div>
         </div>
